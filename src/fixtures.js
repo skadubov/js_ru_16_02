239c61c89c22648dafc7c6b6ad31aba81a930ac0
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-02-26 19:37:49 +0200

    initial commit

---
+++ src/fixtures.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        title: 'My first article',
+        body: 'Lorem Ipsum',
+        id: 1,
+        comments: [
+            {text: 'hello', id: 1}
+        ]
+    },{
+        title: 'Other Article',
+        body: 'Some other text',
+        id: 2
+    },{
+        title: 'Other Article!!!!',
+        body: 'Some other text',
+        id: 3
+    },{
+        title: 'Other Article????',
+        body: 'Some other text',
+        id: 4
+    }
+]
