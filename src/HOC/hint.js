bf72fea3ce7217ce9113c599339cf98a718dc454
skadubov <sky.js.dev@gmail.com>
2016-03-03 11:42:36 +0100

    start state before hometask-01.03

--- src/HOC/hint.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default function (CustomComponent) {
-    return class extends Component {
-        render() {
-            return <CustomComponent {...this.props} hint="This is hint" />
-        }
-    }
-}
