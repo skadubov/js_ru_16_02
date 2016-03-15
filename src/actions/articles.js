ffd7794a19c1c7ccbe61fc66f033da6a38c14a74
skadubov <sky.js.dev@gmail.com>
2016-03-15 10:37:48 +0100

    add react-select, show article by select filter

--- src/actions/articles.js
+++ src/actions/articles.js
@@ -1,6 +1,6 @@
 import AppDispatcher from '../dispatcher'
-import { DELETE_ARTICLE, LOAD_ALL_ARICLES, LOAD_ARTICLE_BY_ID } from './constants'
-import { loadAll, loadArticleById as loadById} from './api/article'
+import { DELETE_ARTICLE, LOAD_ALL_ARTICLES, LOAD_ARTICLE_BY_ID, LOAD_ARTICLES_TITLES, GET_FILTERED_ARTICLES } from './constants'
+import { loadAll, loadArticleById as loadById, loadArticlesTitles as loadTitles} from './api/article'
 import { asyncAC } from './api/utils'
 
 export function deleteArticle(id) {
@@ -10,5 +10,13 @@ export function deleteArticle(id) {
     })
 }
 
-export const loadAllArticles = asyncAC(LOAD_ALL_ARICLES, loadAll)
+export function filteredArticles(list) {
+    AppDispatcher.dispatch({
+        type: GET_FILTERED_ARTICLES,
+        data: { list }
+    })
+}
+
+export const loadAllArticles = asyncAC(LOAD_ALL_ARTICLES, loadAll)
 export const loadArticleById = asyncAC(LOAD_ARTICLE_BY_ID, loadById)
+export const loadArticlesTitles = asyncAC(LOAD_ARTICLES_TITLES, loadTitles)
